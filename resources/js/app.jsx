9fd9e5f2260a21d9eac6b7a5c3712a396f62a4ab
Maxy025 <juanpavo_11@hotmail.com>
2025-10-07 21:28:39 -0600

    [AGREGAR] Instalacion de booststrap vanilla

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,5 +1,7 @@
-import '../css/app.css';
 import './bootstrap';
+import '../scss/app.scss';
+import 'bootstrap';
+
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
@@ -23,3 +25,5 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
+
+
